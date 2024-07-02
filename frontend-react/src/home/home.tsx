f23afd1aabc3920061b8b7578cd45db66c038d3a
KwesiSai <kwesiabraham4503@gmail.com>
2024-07-02 03:52:17 +0000

    Initial content for the home component

---
+++ frontend-react/src/home/home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <h1>Welcome to the <span>FrontEnd Quiz!</span></h1>
+      <p> pick a subject to get started</p>
+    </div>   
+  );
+}
+
+export default Home;
