b99456f12da91f861547eae131a11198e605151d
KwesiSai <kwesiabraham4503@gmail.com>
2024-07-04 13:59:33 +0000

    update: 99% quiz interface done

--- frontend-react/src/quiztest/quiz.tsx
+++ frontend-react/src/quiztest/quiz.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './quiz.css';
 import correct from '../images/icon-correct.svg';
 import wrong from '../images/icon-incorrect.svg';
+import error from '../images/icon-error.svg';
 
 interface Question {
   question: string;
@@ -26,6 +27,11 @@ const Quiz: React.FC<QuizProps> = ({ topic }) => {
   const [score, setScore] = useState(0);
   const [submitted, setSubmitted] = useState(false);
   const [isCorrect, setIsCorrect] = useState(false);
+  const [title, setTitle] = useState<string>("");
+  const [iconSrc, setIconSrc] = useState<string>("");
+  const [totalQuestions] = useState(10);
+  const [progress, setProgress] = useState(0);
+  const [showError, setShowError] = useState(false);
 
   useEffect(() => {
     fetch('/data.json')
@@ -34,6 +40,8 @@ const Quiz: React.FC<QuizProps> = ({ topic }) => {
         const selectedQuiz = data.quizzes.find((quiz: QuizData) => quiz.title.toLowerCase() === topic.toLowerCase());
         if (selectedQuiz) {
           setQuestions(selectedQuiz.questions);
+          setTitle(selectedQuiz.title);
+          setIconSrc(`${process.env.PUBLIC_URL}${selectedQuiz.icon}`);
         }
       })
       .catch(error => console.error('Error fetching questions:', error));
@@ -41,119 +49,91 @@ const Quiz: React.FC<QuizProps> = ({ topic }) => {
 
   const handleOptionClick = (option: string) => {
     setSelectedOption(option);
+    setShowError(false);
   };
 
   const handleSubmitAnswer = () => {
-    if (selectedOption) {
-      if (selectedOption === questions[currentQuestionIndex].answer) {
-        const isCorrect = selectedOption === questions[currentQuestionIndex].answer;
-        setIsCorrect(isCorrect);
-        setScore(score + 1);
-      }
-      else{
-        setIsCorrect(false);
-      }
-      setSubmitted(true);
+    if (!selectedOption) {
+      setShowError(true);
+      return;
     }
+
+    if (selectedOption === questions[currentQuestionIndex].answer) {
+      setIsCorrect(true);
+      setScore(score + 1);
+    } else {
+      setIsCorrect(false);
+    }
+    setSubmitted(true);
   };
 
   const handleNextQuestion = () => {
+    if (!selectedOption) {
+      setShowError(true);
+      return;
+    }
+
     setSelectedOption(null);
     setSubmitted(false);
     setCurrentQuestionIndex(currentQuestionIndex + 1);
     setIsCorrect(false);
+    const newProgress = ((currentQuestionIndex + 1) / totalQuestions) * 100;
+    setProgress(newProgress);
+    setShowError(false);
   };
 
   return (
-    <div className="quiz-page">
-      {questions.length > 0 ? (
-        <>
-          <div className="question-section">
-            <div className="question-count">
-              <span>Question {currentQuestionIndex + 1}</span> of {questions.length}
+    <>
+      <div className="title">
+        <img src={iconSrc} alt="icon" />
+        <span>{title}</span>
+      </div>
+      <div className="quiz-page">
+        {questions.length > 0 ? (
+          <>
+            <div className="question-section">
+              <div className="question-count">
+                <span>Question {currentQuestionIndex + 1}</span> of {questions.length}
+              </div>
+              <div className="question-text">{questions[currentQuestionIndex].question}</div>
+              <div className="bar">
+                <div className="progress" style={{ width: `${progress}%` }} />
+              </div>
             </div>
-            <div className="question-text">{questions[currentQuestionIndex].question}</div>
-          </div>
-          <div className="answer-section">
-            <button tabIndex={0}
-              onClick={() => handleOptionClick(questions[currentQuestionIndex].options[0])}
-              className={`option-button ${submitted && selectedOption === questions[currentQuestionIndex].options[0] ? 'selected' : ''} ${submitted && questions[currentQuestionIndex].options[0] === questions[currentQuestionIndex].answer ? 'correct' : 'wrong'}`}
-              disabled={submitted} // Disable options if submitted
-            >
-              <span>A</span>
-              <p>{questions[currentQuestionIndex].options[0]}</p>
-              {submitted && selectedOption === questions[currentQuestionIndex].options[0] &&(
-                <img src={isCorrect ? correct : wrong} alt={isCorrect ? 'Correct' : 'Wrong'} />
-              )}
-              {submitted && selectedOption !== questions[currentQuestionIndex].options[0] 
-              && questions[currentQuestionIndex].options[0] === questions[currentQuestionIndex].answer
-              &&(
-                <img src={correct} alt='correct' />
-              )}
-            </button>
-
-            <button tabIndex={1}
-              onClick={() => handleOptionClick(questions[currentQuestionIndex].options[1])}
-              className={`option-button ${submitted && selectedOption === questions[currentQuestionIndex].options[1] ? 'selected' : ''} ${submitted && questions[currentQuestionIndex].options[1] === questions[currentQuestionIndex].answer ? 'correct' : 'wrong'}`}
-              disabled={submitted} // Disable options if submitted
-            >
-             <span>B</span>
-             <p>{questions[currentQuestionIndex].options[1]}</p>
-             {submitted && selectedOption === questions[currentQuestionIndex].options[1] &&(
-                <img src={isCorrect ? correct : wrong} alt={isCorrect ? 'Correct' : 'Wrong'} />
-              )}
-              {submitted && selectedOption !== questions[currentQuestionIndex].options[1] 
-              && questions[currentQuestionIndex].options[1] === questions[currentQuestionIndex].answer
-              &&(
-                <img src={correct} alt='correct' />
-              )}
-            </button>
-
-            <button tabIndex={2}
-              onClick={() => handleOptionClick(questions[currentQuestionIndex].options[2])}
-              className={`option-button ${submitted && selectedOption === questions[currentQuestionIndex].options[2] ? 'selected' : ''} ${submitted && questions[currentQuestionIndex].options[2] === questions[currentQuestionIndex].answer ? 'correct' : 'wrong'}`}
-              disabled={submitted} // Disable options if submitted
-            >
-              <span>C</span>
-              <p>{questions[currentQuestionIndex].options[2]}</p>
-              {submitted && selectedOption === questions[currentQuestionIndex].options[2] &&(
-                <img src={isCorrect ? correct : wrong} alt={isCorrect ? 'Correct' : 'Wrong'} />
-              )}
-              {submitted && selectedOption !== questions[currentQuestionIndex].options[2] 
-              && questions[currentQuestionIndex].options[2] === questions[currentQuestionIndex].answer
-              &&(
-                <img src={correct} alt='correct' />
-              )}
-            </button>
-
-            <button tabIndex={3}
-              onClick={() => handleOptionClick(questions[currentQuestionIndex].options[3])}
-              className={`option-button ${submitted && selectedOption === questions[currentQuestionIndex].options[3] ? 'selected' : ''} ${submitted && questions[currentQuestionIndex].options[3] === questions[currentQuestionIndex].answer ? 'correct' : 'wrong'}`}
-              disabled={submitted} // Disable options if submitted
-            >
-              <span>D</span>
-              <p>{questions[currentQuestionIndex].options[3]}</p>
-              {submitted && selectedOption === questions[currentQuestionIndex].options[3] &&(
-                <img src={isCorrect ? correct : wrong} alt={isCorrect ? 'Correct' : 'Wrong'} />
-              )}
-              {submitted && selectedOption !== questions[currentQuestionIndex].options[3] 
-              && questions[currentQuestionIndex].options[3] === questions[currentQuestionIndex].answer
-              &&(
-                <img src={correct} alt='correct' />
-              )}
-            </button>
-
-              <button tabIndex={4} className="submit-button" onClick={submitted ? handleNextQuestion : handleSubmitAnswer}>
+            <div className="answer-section">
+              {questions[currentQuestionIndex].options.map((option, index) => (
+                <button
+                  key={index}
+                  onClick={() => handleOptionClick(option)}
+                  className={`option-button ${submitted && selectedOption === option ? 'selected' : ''} ${
+                    submitted && option === questions[currentQuestionIndex].answer ? 'correct' : 'wrong'
+                  }`}
+                  disabled={submitted}
+                >
+                  <span>{String.fromCharCode(65 + index)}</span>
+                  <p>{option}</p>
+                  {submitted && selectedOption === option && (
+                    <img src={isCorrect ? correct : wrong} alt={isCorrect ? 'Correct' : 'Wrong'} />
+                  )}
+                  {submitted && selectedOption !== option && option === questions[currentQuestionIndex].answer && (
+                    <img src={correct} alt="correct" />
+                  )}
+                </button>
+              ))}
+              <button className="submit-button" onClick={submitted ? handleNextQuestion : handleSubmitAnswer}>
                 {submitted ? 'Next Question' : 'Submit Answer'}
               </button>
-  
-          </div>
-
-        </>
-      ) : (
-        <div></div>
-      )}
-    </div>
+              {showError && <div className="error-message">
+                <img src={error} alt='error'/>
+                Please select an option
+                </div>}
+            </div>
+          </>
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </>
   );
 };
 
